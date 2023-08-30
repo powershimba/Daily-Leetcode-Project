8e7a7008e5e8032934cd592727784416471df9ad
powershimba <powershimba@gmail.com>
2023-08-29 22:49:54 -0700

    2nd Problem Solving

--- python/1480_Running_Sum_of_1d_Array.py
+++ python/1480_Running_Sum_of_1d_Array.py
@@ -1,3 +1,12 @@
+"""
+1480. Running Sum of 1d Array
+
+Given an array nums. 
+We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+"""
+
+
 class Solution(object):
     def runningSum(self, nums):
         output = []

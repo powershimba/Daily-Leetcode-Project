36ef42cfe9a3d955cb08ba4ece7541c698dca8a7
powershimba <powershimba@gmail.com>
2023-09-17 18:36:35 -0700

    Need to RETRY

--- python/819_Most_Common_Word.py
+++ python/819_Most_Common_Word.py
@@ -1,21 +1,10 @@
 class Solution(object):
     def mostCommonWord(self, paragraph, banned):
-        paragraph = paragraph.lower()
-        for char in ',.':
-            paragraph = paragraph.replace(char, "")
-        words = paragraph.split()
-
-        dict = {}
-        for word in words:
-            if word not in banned:
-                if word not in dict:
-                    dict[word] = 1
-                else:
-                    dict[word] += 1
+        import collections
+        words = [word for word in re.sub(r'[^\w]', " ", paragraph).lower().split() if word not in banned]
+        counts = collections.Counter(words)
+        return counts.most_common(1)[0][0]
         
-        max_key = max(dict, key=lambda k: dict[k])
-        return max_key
-
         """
         :type paragraph: str
         :type banned: List[str]

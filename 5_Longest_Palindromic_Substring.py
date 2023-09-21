121d3c95ff072d7c056a9f31648043cf48fa147f
powershimba <powershimba@gmail.com>
2023-09-21 16:26:29 -0700

    Important

---
+++ 5_Longest_Palindromic_Substring.py
@@ -0,0 +1,26 @@
+# Brutal Force Solution : O(n3)
+# My Solution : O(n2)
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        def expand(i, j):
+            while i>= 0 and j<len(s) and s[i] == s[j]:
+                i = i-1
+                j = j+1
+            return s[i+1:j]
+        
+        final = ""
+        for i in range(len(s)):
+            pan1 = expand(i,i) # start from one letter
+            if len(pan1) > len(final):
+                final = pan1
+            pan2 = expand(i, i+1) # start from the center between two letters
+            if len(pan2) > len(final):
+                final = pan2
+        return final
+
+        """
+        :type s: str
+        :rtype: str
+        """
+        

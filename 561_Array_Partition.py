f1f1d476acc68af1d6c58a5861d837a4c5482af0
powershimba <powershimba@gmail.com>
2023-09-24 11:03:01 -0700

    easy(add solution)

--- 561_Array_Partition.py
+++ 561_Array_Partition.py
@@ -6,6 +6,7 @@ class Solution(object):
         for i in range(len(nums)-1):
             if i==0 or i%2 == 0:
                 sum += nums[i]
+        # sum(nums[i] for i in range(0, len(nums), 2))
         return sum
         """
         :type nums: List[int]

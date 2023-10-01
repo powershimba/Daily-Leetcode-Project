2a16def1e4b426295ee95aa666c813336f775a4f
powershimba <powershimba@gmail.com>
2023-10-01 12:52:07 -0700

    Linked List Swaping(Recursion)

--- 24_Swap_Nodes_in_Pairs.py
+++ 24_Swap_Nodes_in_Pairs.py
@@ -9,14 +9,42 @@ Output: [2,1,4,3]
 """
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-# Iterately Swap
+# Recursive
+# Time Complexity: O(n) / 14ms(65.15%)
+# Memory Complexity: O(1) / 13.13MB(91.06%)
+class Solution(object):
+    def swapPairs(self, head):
+        if head and head.next:
+            n = head.next
+            head.next = self.swapPairs(n.next)
+            n.next = head
+            return n
+        return head
+
+# Iterately Swap with Dummy Node for indicating first node
+# Time Complexity: O(n) / 16ms(44.63%)
+# Memory Complexity: O(1) / 13.48MB(11.76%)
+class Solution(object):
+    def swapPairs(self, head):
+        root = prev = ListNode(None)
+        prev.next = head
+
+        while head and head.next:
+            ptr = head.next
+            ptr.next, head.next, prev.next = head, ptr.next, ptr
+            head, prev = head.next, prev.next.next
+
+        return root.next
+
+
+# Iterately Swap with Switch for indicating first node
 # Time Complexity: O(n) / 17ms(40.74%)
-# Memory Complexity: O(n) / 13.08MB(97.78%)
+# Memory Complexity: O(1) / 13.08MB(97.78%)
 class Solution(object):
     def swapPairs(self, head):
         node = prev = head
@@ -27,6 +55,7 @@ class Solution(object):
         #when swap
         while node and node.next:
             node = node.next
+
             if swap == 0:
                 root.next = node
                 swap = 1
@@ -42,11 +71,3 @@ class Solution(object):
                 prev = node = next
             
         return root.next
-
-
-
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        

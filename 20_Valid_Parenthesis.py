87e89a4cdd5a1ae00dc4e2f5a024ea9e280dedb4
powershimba <powershimba@gmail.com>
2023-10-05 23:24:20 -0700

    Easy, Stack

--- 20_Valid_Parenthesis.py
+++ 20_Valid_Parenthesis.py
@@ -3,6 +3,26 @@ Easy
 20. Valid Parentheses
 """
 
+# Mapping
+# Time: O(n) / 10ms(90.46%)
+# Memory: O(n) / 13.29MB(89.79%)
+
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        table = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '[',
+        }
+        for char in s:
+            if char not in table: # (, {, [
+                stack.append(char)
+            elif not stack or (table[char] != stack.pop()): # ), }, ] 
+                return False
+        return len(stack) == 0
+
+# Iterative
 # Time: O(n) / 15ms(55.25%)
 # Memory: O(n) / 13.73MB(13.75%)
 
@@ -23,9 +43,3 @@ class Solution(object):
                     return False
         if stack == []:
             return True 
-
-        """
-        :type s: str
-        :rtype: bool
-        """
-        

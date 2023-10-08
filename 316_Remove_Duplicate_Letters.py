75daa52ee7c4d0eb72baa66048cd929bbec3d84d
powershimba <powershimba@gmail.com>
2023-10-07 23:58:23 -0700

    review required

--- 316_Remove_Duplicate_Letters.py
+++ 316_Remove_Duplicate_Letters.py
@@ -9,4 +9,10 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        
+        # sort s as set
+        for char in sorted(set(s)):
+            # seperate if set of suffix(include char) set is same as entire set
+            suffix = s[s.index(char):]
+            if set(s) == set(suffix):
+                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
+        return ''                        

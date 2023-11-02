c33e505b4c45c8be757f5fc95240e758eaa6334a
powershimba <powershimba@gmail.com>
2023-11-01 18:54:25 -0700

    Merge by Divide and Conquer

---
+++ 23_Merge_k_Sorted_Lists.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Merge by Divide and Conquer
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        
+        mid = len(lists) // 2
+        left = self.mergeKLists(lists[:mid])
+        right = self.mergeKLists(lists[mid:])
+
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        head = ListNode()
+        curr = head
+
+        while left and right:
+            if left.val < right.val:
+                curr.next = left
+                left = left.next
+            else:
+                curr.next = right
+                right = right.next
+            curr = curr.next
+        
+        curr.next = left or right
+
+        return head.next
